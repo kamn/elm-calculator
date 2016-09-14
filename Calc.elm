298b23e965740ead8149c04aef05503a8124ba58
Kamn <kamn@users.noreply.github.com>
2016-09-13 20:42:00 -0600

    Added basic buttons

--- Calc.elm
+++ Calc.elm
@@ -1,6 +1,17 @@
 module Calc exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, div, button)
 
 main =
-  text "Calculator"
+  div [] [
+    text "Calculator",
+    div [] [button [] [text "1"],
+            button [] [text "2"],
+            button [] [text "3"]],
+    div [] [button [] [text "4"],
+            button [] [text "5"],
+            button [] [text "6"]],
+    div [] [button [] [text "7"],
+            button [] [text "8"],
+            button [] [text "9"]],
+    div [] [button [] [text "0"]]]

ad6f4ebf816750b627bdb3397a3617fe31cc82e6
Kamn <kamn@users.noreply.github.com>
2016-09-14 22:24:59 -0600

    Added events and model to always be linked with button pressed

--- Calc.elm
+++ Calc.elm
@@ -1,7 +1,9 @@
 module Calc exposing (..)
 
-import Html exposing (text, div, button)
+import Html exposing (Html, text, div, button)
+import Html.App as App
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
 
 centerStyle : List (String, String)
 centerStyle =
@@ -10,17 +12,40 @@ centerStyle =
     ("justify-content", "center"),
     ("height", "100vh")]
 
-main =
+-- MODEL
+type alias Model = Int
+
+model: Model
+model =
+  0
+
+-- Update
+
+type alias Msg = Int
+
+update: Msg -> Model -> Model
+update msg model =
+  msg
+
+
+-- VIEW
+
+view: Model -> Html Msg
+view model =
   div [style centerStyle] [
     div [] [
       text "Calculator",
-      div [] [button [] [text "1"],
-              button [] [text "2"],
-              button [] [text "3"]],
-      div [] [button [] [text "4"],
-              button [] [text "5"],
-              button [] [text "6"]],
-      div [] [button [] [text "7"],
-              button [] [text "8"],
-              button [] [text "9"]],
-      div [] [button [] [text "0"]]]]
+      div [] [text (toString model)],
+      div [] [button [onClick 1] [text "1"],
+              button [onClick 2] [text "2"],
+              button [onClick 3] [text "3"]],
+      div [] [button [onClick 4] [text "4"],
+              button [onClick 5] [text "5"],
+              button [onClick 6] [text "6"]],
+      div [] [button [onClick 7] [text "7"],
+              button [onClick 8] [text "8"],
+              button [onClick 9] [text "9"]],
+      div [] [button [onClick 0] [text "0"]]]]
+
+main =
+  App.beginnerProgram { model = model, view = view, update = update }
